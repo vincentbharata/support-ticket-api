a32ff90da433efa9362912f96ffa7d94d9615f09
vincentbharata <vincentbharataa@gmail.com>
2025-06-17 13:38:29 +0700

    init

---
+++ src/main/java/com/example/support_ticket/SupportTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.support_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/support_ticket/SupportTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.support_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
